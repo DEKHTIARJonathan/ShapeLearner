3089982a87aef8f534ff6ddf1bef2dabf03b6a2f
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2015-04-27 15:16:43 +0200

    testproject heritage testing

--- testProject/test/main.cpp
+++ testProject/test/main.cpp
@@ -5,50 +5,31 @@
 #include <string>
 using namespace std;
 
+class A{
+protected:
+	int a;
+public:
+	A() { a = 4;}
+};
+
+class B : public A {
+	int &b;
+public:
+	B() : b(a){}
+	void test()  {
+		std::cout<<"a = " << a << " and b = " << b <<endl ;
+		a = 5;
+		std::cout<<"a = " << a << " and b = " << b <<endl ;
+		b = 6;
+		std::cout<<"a = " << a << " and b = " << b <<endl ;
+	}
+};
+
 int main () {
-	typedef std::shared_ptr<string> strPtr;
+	B tst;
 
-	map <int, strPtr> index;
+	tst.test();
+	system("pause");
 
-	index.insert(std::pair<int, strPtr>(1,(strPtr)new string("Hello")));
-	index.insert(std::pair<int, strPtr>(2,(strPtr)new string("My")));
-	index.insert(std::pair<int, strPtr>(3,(strPtr)new string("Friend")));
-
-	std::cout << "Index contains:\n";
-	for (std::map <int, strPtr>::iterator it = index.begin(); it!=index.end(); ++it)
-		std::cout << it->first << " => " << *(it->second) << '\n';
-	cout << endl;
-
-	std::weak_ptr<string> weak(index[2]);
-
-	std::cout << "1. weak " << (weak.expired()?"is":"is not") << " expired\n";
-
-	index[2].reset();
-
-	std::cout << "2. weak " << (weak.expired()?"is":"is not") << " expired\n";
-
-	cout << " ============================== 2nd PART =============================" << endl ;
-
-	map <string, strPtr> indexStr;
-
-	indexStr.insert(std::pair<string, strPtr>("1st",(strPtr)new string("Hello")));
-	indexStr.insert(std::pair<string, strPtr>("2nd",(strPtr)new string("My")));
-	indexStr.insert(std::pair<string, strPtr>("3rd",(strPtr)new string("Friend")));
-
-	std::cout << "Index contains:\n";
-	for (std::map <string, strPtr>::iterator it = indexStr.begin(); it!=indexStr.end(); ++it)
-		std::cout << it->first << " => " << *(it->second) << '\n';
-	cout << endl;
-
-	std::weak_ptr<string> weakStr(indexStr["2nd"]);
-
-	std::cout << "1. weak " << (weakStr.expired()?"is":"is not") << " expired\n";
-
-	indexStr["2nd"].reset();
-
-	std::cout << "2. weak " << (weakStr.expired()?"is":"is not") << " expired\n";
-
-  system("pause");
-
-  return 0;
+	return 0;
 }
